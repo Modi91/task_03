97bf22e6f9d618fc14f395744286b2a207a8ae14
Modi91 <alotaibimood@gmail.com>
2019-02-10 17:07:26 +0300

    task3

--- restaurants/views.py
+++ restaurants/views.py
@@ -5,7 +5,18 @@ def welcome(request):
 
 def restaurant_list(request):
 
-    context = {
+    context = { 
+    'my_list':[{
+    'restaurant_name': 'Modi',
+    'food_type': 'Traditional Food'},
+
+    {'restaurant_name': 'Italy',
+    'food_type': 'Italian Food'},
+
+    {'restaurant_name': 'Arabic',
+    'food_type': 'Arabian Food'}
+    ]
+    
 
     }
     return render(request, 'list.html', context)
@@ -14,6 +25,10 @@ def restaurant_list(request):
 def restaurant_detail(request):
 
     context = {
-
+    'my_object': {'restaurant_name': 'Modi',
+                  'food_type': 'Traditional Food'
+                  }
     }
+
+    
     return render(request, 'detail.html', context)
